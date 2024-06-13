c818742da74e4ff64f722e2b1d319e4b7ace1f17
danielkabok <172530811+danielkabok@users.noreply.github.com>
2024-06-13 11:58:15 +0200

    feat: Create basic views

---
+++ src/views/AdvertisementsView.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Advertisements</title>
+</head>
+<body>
+    <div>
+        <h1>Advertisements</h1>
+        <p><a href="/php-mvc-app/index">Back to index</a></p>
+    </div>
+</body>
+</html>
---
+++ src/views/IndexView.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Index</title>
+</head>
+<body>
+    <div>
+        <h1>Index</h1>
+        <p><a href="/php-mvc-app/user">Users</a></p>
+        <p><a href="/php-mvc-app/advertisement">Advertisements</a></p>
+    </div>
+</body>
+</html>
