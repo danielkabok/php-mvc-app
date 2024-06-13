420abd6d2d8fe2b8e4dcc5876a311562cdae47c9
danielkabok <172530811+danielkabok@users.noreply.github.com>
2024-06-13 21:12:41 +0200

    feat: Hide ID columns on user and advertisement pages

--- src/views/AdvertisementsView.php
+++ src/views/AdvertisementsView.php
@@ -11,13 +11,13 @@
         <p><a href="/php-mvc-app/index">Back to index</a></p>
         <table>
             <tr>
-                <th>ID</th>
+                <!-- <th>ID</th> -->
                 <th>User</th>
                 <th>Title</th>
             </tr>
             <?php foreach ($advertisements as $advertisement): ?>
             <tr>
-                <td><?php echo htmlspecialchars($advertisement->id); ?></td>
+                <!-- <td><?php echo htmlspecialchars($advertisement->id); ?></td> -->
                 <?php
                     $user = $userService->getUserById($advertisement->userid);
                     $userName = $user->name;
--- src/views/UsersView.php
+++ src/views/UsersView.php
@@ -11,12 +11,12 @@
         <p><a href="/php-mvc-app/index">Back to index</a></p>
         <table>
             <tr>
-                <th>ID</th>
+                <!-- <th>ID</th> -->
                 <th>Name</th>
             </tr>
             <?php foreach ($users as $user): ?>
             <tr>
-                <td><?php echo htmlspecialchars($user->id); ?></td>
+                <!-- <td><?php echo htmlspecialchars($user->id); ?></td> -->
                 <td><?php echo htmlspecialchars($user->name); ?></td>
             </tr>
             <?php endforeach; ?>
